8a14646e42f60d2f309aa945085245398cdaceeb
Intesar Khan <intesarkhan@virtualemployee.com>
2026-03-03 01:19:53 +0530

    change

--- app/sitemap-world-index.xml/route.ts
+++ app/sitemap-world-index.xml/route.ts
@@ -1,27 +1,30 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const baseUrl =
+    process.env.NEXT_PUBLIC_BASE_URL?.replace(/\/+$/, "") ||
+    "https://isevenplus.com";
+
   const now = new Date().toISOString();
+
+  const sitemaps = [
+    "sitemap-world-1.xml",
+    "sitemap-world-2.xml",
+    "sitemap-world-3.xml",
+    "sitemap-world-4.xml",
+  ];
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${sitemaps
+  .map(
+    (s) => `
   <sitemap>
-    <loc>${baseUrl}/sitemap-world-1.xml</loc>
+    <loc>${new URL(`/${s}`, baseUrl).toString()}</loc>
     <lastmod>${now}</lastmod>
-  </sitemap>
-  <sitemap>
-    <loc>${baseUrl}/sitemap-world-2.xml</loc>
-    <lastmod>${now}</lastmod>
-  </sitemap>
-  <sitemap>
-    <loc>${baseUrl}/sitemap-world-3.xml</loc>
-    <lastmod>${now}</lastmod>
-  </sitemap>
-  <sitemap>
-    <loc>${baseUrl}/sitemap-world-4.xml</loc>
-    <lastmod>${now}</lastmod>
-  </sitemap>
+  </sitemap>`
+  )
+  .join("")}
 </sitemapindex>`;
 
   return new NextResponse(xml, {
