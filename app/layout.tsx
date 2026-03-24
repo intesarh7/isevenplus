953c7f2fdf6ce0cde55c40c034a9a61719361b5b
Intesar Khan <intesarkhan@virtualemployee.com>
2026-03-24 20:44:41 +0530

    g tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -11,12 +12,12 @@ const inter = Inter({
 ============================================ */
 
 const baseUrl =
-  process.env.NEXT_PUBLIC_BASE_URL || "http://www.localhost:3000";
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 export const metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: "iSevenPlus – Smart Online Tools & Pincode Finder",
+    default: "iSevenPlus - Smart Online Tools & Pincode Finder",
     template: "%s | iSevenPlus",
   },
   description:
@@ -34,7 +35,22 @@ export default function RootLayout({
         <meta name="google-site-verification" content="JzIfzUsG3INxXqkQyVOFVOjm-Htlv49E44OMCobqkUo" />
         <meta name="msvalidate.01" content="355F9526F1FA6716B94F6FD3ADE9B3DA" />
         <meta name="google-adsense-account" content="ca-pub-5131122927046742" />
+
+        {/* ✅ Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-TF1NBTNYH0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TF1NBTNYH0');
+          `}
+        </Script>
       </head>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
