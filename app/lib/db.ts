69f75445224f51f606e975dae4cd6ce8d89a0470
Intesar Haider <intesarh7@gmail.com>
2026-03-30 02:28:16 +0530

    Update db.ts

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -14,7 +14,7 @@ const pool =
     port: Number(process.env.DB_PORT) || 3306,
 
     waitForConnections: true,
-    connectionLimit: 1, // 🔥 IMPORTANT (zyada mat rakho)
+    connectionLimit: 10, // 🔥 IMPORTANT (zyada mat rakho)
     queueLimit: 0,
 
     connectTimeout: 10000,
