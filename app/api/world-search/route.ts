e6ed33c1f5e065ba4ae814913159553f091cd3b8
Intesar Haider <intesarh7@gmail.com>
2026-03-30 01:04:25 +0530

    Update route.ts

--- app/api/world-search/route.ts
+++ app/api/world-search/route.ts
@@ -1,6 +1,7 @@
 import db from "@/app/lib/db";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
 /* ================================
    NORMALIZE TEXT (SEARCH SAFE)
 ================================ */
