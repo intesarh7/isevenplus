633ba7728ae08bab0599983e9c897550ce355392
Intesar Khan <intesarkhan@virtualemployee.com>
2026-04-01 21:55:57 +0530

    update

--- app/(site)/postalcode/page.tsx
+++ app/(site)/postalcode/page.tsx
@@ -20,7 +20,7 @@ export const dynamic = "force-dynamic";
 /* ================================
    CACHE
 ================================ */
-export const revalidate = 3600;
+export const revalidate = 0;
 
 /* ================================
    HELPER
